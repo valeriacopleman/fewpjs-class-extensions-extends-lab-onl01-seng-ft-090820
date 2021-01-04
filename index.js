9c79b25e1038690a37e37f2d403373eb4435aa1e
Valeria Copleman <valeriacopleman@gmail.com>
2021-01-03 18:52:33 -0800

    commit tree e2d8e2103d8c5bb3649605c165c7e7b31ac6308b

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+class Polygon {
+    constructor(sides = []) {
+        this.sides = sides
+    }
+    get countSides() {
+        return this.sides.length;
+    }
+    get perimeter() {
+        const add = (a, b) => a + b
+        return this.sides.reduce(add);
+    }
+}
+
+class Triangle extends Polygon {
+
+}
+
+class Square extends Polygon {
+    get isValid() {
+        return !!(this.sides[0] == this.sides[1] == this.sides[2] == this.sides[3]);
+    }
+    get area() {
+        return this.sides[0] * this.sides[1]
+    }
+}
